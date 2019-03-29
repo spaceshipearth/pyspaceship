0b0fe3790520ea0850bd2e4d2816ccb1b5a0f3fd
igor <igor47@moomers.org>
2019-03-28 20:44:15 -0700

    invalid form submit js
    copied straight outta bootstrap docs

---
+++ spaceship/static/prevent-invalid-form-submit.js
@@ -0,0 +1,18 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', () => {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    const validation = Array.prototype.filter.call(forms, (form) => {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
