5dc06ade2528102c1c8e8494572d8b1cb8e00bfe
Igor Serebryany <igor47@moomers.org>
2019-04-25 11:32:04 -0700

    better lb log printing
    don't always print agent, which makes log easier to read. also, handle
    disconnections (where there is no status code or response size)

--- tasks/logs.py
+++ tasks/logs.py
@@ -7,9 +7,10 @@ import pendulum
 @task(
   help={
     'freshness': 'Interval over which to look at logs (default: "1h")',
+    'agent': 'Print the user agent',
   },
 )
-def lb(ctx, freshness='1h'):
+def lb(ctx, freshness='1h', agent=False):
   """Logs from the load balancer (ingress)"""
   output = json.loads(
     run("gcloud logging read 'resource.type=http_load_balancer'"
@@ -18,17 +19,26 @@ def lb(ctx, freshness='1h'):
         hide=True).stdout)
 
   for line in output[::-1]:
-    time = pendulum.parse(line['timestamp'])
-    req = line['httpRequest']
-    print(" ".join([
-      time.to_datetime_string(),
-      req['requestMethod'],
-      str(req['status']),
-      req['responseSize'],
-      req['requestUrl'],
-      req['userAgent'],
-      req['remoteIp'],
-    ]))
+    try:
+      time = pendulum.parse(line['timestamp'])
+      req = line['httpRequest']
+      status = str(req['status']) if 'status' in req else '???'
+
+      parts = [
+        time.to_datetime_string(),
+        req['remoteIp'],
+        req['requestMethod'],
+        status,
+        req['responseSize'] if 'responseSize' in req else '0',
+        req['requestUrl'],
+      ]
+
+      if agent:
+        parts.append(req['userAgent'])
+
+      print(" ".join(parts))
+    except Exception as e:
+      print(f"Invalid log line {line}: {e}")
 
 @task
 def logs(ctx):
