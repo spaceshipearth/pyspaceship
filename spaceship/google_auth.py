1b4ceaf9f71bcbf27520f450d4e57d606178e51a
Igor Serebryany <igor47@moomers.org>
2019-04-25 20:40:51 -0700

    handle the login by creating/looking up user

--- spaceship/google_auth.py
+++ spaceship/google_auth.py
@@ -1,6 +1,10 @@
 from authlib.flask.client import OAuth
 from loginpass import create_flask_blueprint, Google
 
+from flask_login import login_user
+from flask import redirect, url_for
+
+from .models.user import User
 from . import app
 oauth = OAuth(app)
 
@@ -8,8 +12,17 @@ def handle_authorize(remote, token, user_info):
   if token:
     print(remote.name, token)
   if user_info:
-    print(f'should save {user_info}')
-  return f'<html>{user_info}</html>'
+    email = user_info['email']
+
+    try:
+      user = User.get(User.email == email)
+    except User.DoesNotExist:
+      user = User(email=email)
+      user.save()
+
+    login_user(user)
+
+  return redirect(url_for('dashboard'))
 
 google_bp = create_flask_blueprint(Google, oauth, handle_authorize)
 app.register_blueprint(google_bp, url_prefix='/google')
