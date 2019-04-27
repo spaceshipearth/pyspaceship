3d2996b083608a354aa62c256c5981f984016876
Igor Serebryany <igor47@moomers.org>
2019-04-27 16:12:57 -0700

    logs: only try to use google logging if we're in production
    we assume that only production has the correct google auth stuff. this
    makes it a bit harder to test locally, but also makes local code run
    much faster since we're not trying to connect to google at app startup

--- spaceship/__init__.py
+++ spaceship/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-from . import logs
-
 from .config import Config
 app.config.from_object(Config)
+
+from . import logs
 
 from flask_wtf.csrf import CSRFProtect
 csrf = CSRFProtect(app)
--- spaceship/logs.py
+++ spaceship/logs.py
@@ -1,16 +1,23 @@
 import google.cloud.logging
 import logging
 
-from google.auth.exceptions import GoogleAuthError
+from . import app
 
 # enable google cloud logging
 # from: https://cloud.google.com/logging/docs/setup/python
-try:
+if app.config['IN_PRODUCTION']:
   client = google.cloud.logging.Client()
   client.setup_logging()
 
+  # set gunicorn logging to what google set up
+  logger = logging.getLogger('spaceship')
+  google_handlers = logger.handlers
+
+  gunicorn_logger = logging.getLogger('gunicorn.error')
+  gunicorn_logger.handlers = google_handlers
+
 # if google logging is not available, set up our own (to stdout)
-except GoogleAuthError:
+else:
   # get our own logger that uses the same level as the app
   logger = logging.getLogger('spaceship')
   logger.setLevel(logging.INFO)
@@ -22,14 +29,6 @@ except GoogleAuthError:
   # our log format
   formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s : %(message)s')
   handler.setFormatter(formatter)
-
-# if google is configured, set gunicorn logging to what google set up
-else:
-  logger = logging.getLogger('spaceship')
-  google_handlers = logger.handlers
-
-  gunicorn_logger = logging.getLogger('gunicorn.error')
-  gunicorn_logger.handlers = google_handlers
 
 # output a message indicating we're booting up
 logging.getLogger('spaceship').warn('spaceship is launching...')
