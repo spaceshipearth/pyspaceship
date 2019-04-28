d8d7cc8402723095559eef313ba628e8b3be2f54
Igor Serebryany <igor47@moomers.org>
2019-04-27 19:10:26 -0700

    better proxy fix
    instead of creating our own middleware app to fix external urls, lets
    use the built-in fix. most of this came from:
    https://stackoverflow.com/a/45333882/153995
    
    and i checked the source to make sure the right thing happens:
    https://github.com/pallets/werkzeug/blob/7fef41b120327d3912fbe12fb64f1951496fcf3e/src/werkzeug/middleware/proxy_fix.py#L140

--- spaceship/enforce_ssl.py
+++ spaceship/enforce_ssl.py
@@ -1,12 +1,13 @@
 
 from flask import request, redirect
-from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from . import app
 
 # first, apply the proxy fix. this makes us be happy if we're behind
 # a proxy and running over https (e.g., fixes url generation)
-app.wsgi_app = ProxyFix(app.wsgi_app)
+# we expect a single forward (from google ingress) so both `for` and `proto` are set to 1
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 # now, if we have a `X-Forwarded-Proto` field that's `http`, redirect to https
 @app.before_request
