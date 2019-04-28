6df9b475b1b63a0228e081078d63f01d873f69f8
Jered Wierzbicki <jeredw@gmail.com>
2019-04-27 17:56:50 -0700

    fix flask to write https urls
    looks like we're behind a reverse proxy which is messing up oauth links

--- spaceship/__init__.py
+++ spaceship/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+
+# this is to make url_for() use https:// for _external urls (like oauth
+# redirect_uri) when deployed behind a load-balancer that talks http
+class ReverseProxied(object):
+  def __init__(self, app):
+    self.app = app
+
+  def __call__(self, environ, start_response):
+    scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+    if scheme:
+      environ['wsgi.url_scheme'] = scheme
+    return self.app(environ, start_response)
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 from .config import Config
 app.config.from_object(Config)
