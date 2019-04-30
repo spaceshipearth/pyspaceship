f779d28f3d9c289ca42b0d099319807612e06d8b
David Ginsburg <david@daviddoodles.com>
2019-04-30 03:21:26 -0700

    Use cookie to only display wip overlay once
    If javascript is enabled, the work-in-progress overlay will be shown once
    every 30 days using a cookie to determine if a user has already seen it. If
    javascript is disabled, the work-in-progress overlay will not be
    displayed.

--- spaceship/static/wip-overlay.js
+++ spaceship/static/wip-overlay.js
@@ -1,20 +1,45 @@
 (function() {
-  var closeOverlay = (event) => {
+  var closeOverlay = (evnt) => {
     document.getElementById('overlay-background').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
-    event.preventDefault();
-    event.stopPropagation();
+    evnt.preventDefault();
+    evnt.stopPropagation();
+  };
+  // cookie functions based on https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript#24103596
+  var setCookie = (name, value, days) => {
+    var expires = '';
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + (value || '')  + expires + '; path=/';
+  };
+  var getCookie = (name) => {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
   };
   window.addEventListener('load', () => {
-    const elmts = document.getElementsByClassName('close-overlay');
-    const handleclicks = Array.prototype.filter.call(elmts, (elmt) => {
-      elmt.addEventListener('click', closeOverlay, false);
-    });
-    document.addEventListener('keydown', (e) => {
-      // close overlay when escape key is pressed
-      if (e.keyCode == 27) {
-        closeOverlay(e);
-      }
-    }, false);
+    if (getCookie('displayWIP') != 'false') {
+      document.getElementById('overlay-background').style.display = 'block';
+      document.getElementById('overlay').style.display = 'block';
+      const elmts = document.getElementsByClassName('close-overlay');
+      const handleclicks = Array.prototype.filter.call(elmts, (elmt) => {
+        elmt.addEventListener('click', closeOverlay, false);
+      });
+      document.addEventListener('keydown', (e) => {
+        // close overlay when escape key is pressed
+        if (e.keyCode == 27) {
+          closeOverlay(e);
+        }
+      }, false);
+      setCookie('displayWIP', 'false', 90);
+    }
   }, false);
 })();
