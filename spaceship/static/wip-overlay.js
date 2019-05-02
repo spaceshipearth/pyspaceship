4121b2b7b8ec5f20c01fa1000fd0621cca0a6011
David Ginsburg <david@daviddoodles.com>
2019-05-02 01:35:50 -0700

    use js-cookie to set and get cookies for wip modal
    Uses cloudflare CDN to source js-cookie. Learn about js-cookie here: https://github.com/js-cookie/js-cookie

--- spaceship/static/wip-overlay.js
+++ spaceship/static/wip-overlay.js
@@ -1,30 +1,10 @@
 (function() {
-  // cookie functions based on https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript#24103596
-  var setCookie = (name, value, days) => {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = '; expires=' + date.toUTCString();
-    }
-    document.cookie = name + '=' + (value || '')  + expires + '; path=/';
-  };
-  var getCookie = (name) => {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
-    for(var i=0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  };
   window.addEventListener('load', () => {
-    if (getCookie('spaceshipearth_display_wip') != 'false') {
+    if (Cookies.get('spaceshipearth_display_wip') != 'false') {
       $('#wipOverlay').modal({
         keyboard: true
       });
-      setCookie('spaceshipearth_display_wip', 'false', 90);
+      Cookies.set('spaceshipearth_display_wip', 'false', { expires: 90, path: '' });
     }
   }, false);
 })();
