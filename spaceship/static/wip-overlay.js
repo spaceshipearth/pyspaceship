2f86a1b4a227b47222c7be1caf8da040f5b082d8
David Ginsburg <david@daviddoodles.com>
2019-05-02 09:14:23 -0700

    use jquery to load wip overlay

--- spaceship/static/wip-overlay.js
+++ spaceship/static/wip-overlay.js
@@ -1,10 +1,8 @@
-(function() {
-  window.addEventListener('load', () => {
-    if (Cookies.get('spaceshipearth_display_wip') != 'false') {
-      $('#wipOverlay').modal({
-        keyboard: true
-      });
-      Cookies.set('spaceshipearth_display_wip', 'false', { expires: 90, path: '' });
-    }
-  }, false);
-})();
+$(window).on('load', function() {
+  if (Cookies.get('spaceshipearth_display_wip') != 'false') {
+    $('#wipOverlay').modal({
+      keyboard: true
+    });
+    Cookies.set('spaceshipearth_display_wip', 'false', { expires: 90, path: '' });
+  }
+});
