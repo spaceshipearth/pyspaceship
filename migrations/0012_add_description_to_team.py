c422fd8a8a7538c0f1aa95e675267e5ab660cc06
Jered Wierzbicki <jeredw@gmail.com>
2019-05-05 14:11:05 -0700

    peewee-moves add_column uses str for type

--- migrations/0012_add_description_to_team.py
+++ migrations/0012_add_description_to_team.py
@@ -2,10 +2,8 @@
 add description to team
 date created: 2019-04-26 06:27:46.338009
 """
-from peewee import TextField
-
 def upgrade(migrator):
-    migrator.add_column('team', 'description', TextField, default='Best. Crew. Ever.')
+    migrator.add_column('team', 'description', 'text', default='Best. Crew. Ever.')
 
 def downgrade(migrator):
     migrator.drop_column('team', 'description')
--- migrations/0016_drop_unused_mission_and_goal_fields.py
+++ migrations/0016_drop_unused_mission_and_goal_fields.py
@@ -2,7 +2,6 @@
 drop unused mission and goal fields
 date created: 2019-05-04 21:44:40.156692
 """
-from peewee import IntegerField, BooleanField
 
 def add_foreign_key(migrator, from_table, from_field, to_table, to_field, **kwargs):
   """Add a foreign key, which playhouse-moves doesn't seem to do properly."""
@@ -18,9 +17,9 @@ def upgrade(migrator):
 
 def downgrade(migrator):
   add_foreign_key(migrator, 'mission', 'prerequisite_id', 'mission', 'id', null=True)
-  migrator.add_column('goal', 'primary_for_mission', BooleanField, default=True)
+  migrator.add_column('goal', 'primary_for_mission', 'bool', default=True)
   add_foreign_key(migrator, 'goal', 'mission_id', 'mission', 'id', null=True)
-  migrator.add_column('goal', 'week', IntegerField, default=0)
+  migrator.add_column('goal', 'week', 'int', default=0)
   migrator.execute_sql(
     '''update goal g
        inner join missiongoal mg on g.id = mg.goal_id
