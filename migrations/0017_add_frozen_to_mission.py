92b6d5e5df2b862a78a22104969ee052a37ff617
Jered Wierzbicki <jeredw@gmail.com>
2019-05-05 14:11:05 -0700

    peewee-moves add_column uses str for type

--- migrations/0017_add_frozen_to_mission.py
+++ migrations/0017_add_frozen_to_mission.py
@@ -3,13 +3,11 @@ add frozen to mission
 date created: 2019-05-05 18:42:58.476185
 """
 
-from peewee import BooleanField, CharField, IntegerField
-
 def upgrade(migrator):
-  migrator.add_column('mission', 'frozen', BooleanField, default=False)
+  migrator.add_column('mission', 'frozen', 'bool', default=False)
   migrator.execute_sql('update mission set frozen=true;')
-  migrator.add_column('mission', 'duration_in_weeks', IntegerField, default=4)
-  migrator.add_column('mission', 'started_at', CharField, null=True)
+  migrator.add_column('mission', 'duration_in_weeks', 'int', default=4)
+  migrator.add_column('mission', 'started_at', 'char', null=True)
 
 def downgrade(migrator):
   migrator.drop_column('mission', 'frozen')
--- migrations/0018_drop_mission_started_at_from_team.py
+++ migrations/0018_drop_mission_started_at_from_team.py
@@ -2,10 +2,8 @@
 drop mission_started_at from team
 date created: 2019-05-05 19:10:41.028519
 """
-from peewee import CharField
-
 def upgrade(migrator):
   migrator.drop_column('team', 'mission_start_at')
 
 def downgrade(migrator):
-  migrator.add_column('team', 'mission_start_at', CharField, null=True)
+  migrator.add_column('team', 'mission_start_at', 'char', null=True)
