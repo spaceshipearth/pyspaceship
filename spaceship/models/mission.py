870daafb322e6d8801b53fa0dc35f00853ce39d5
Jered Wierzbicki <jeredw@gmail.com>
2019-05-11 10:46:04 -0700

    basic ui for goal rescheduling

--- spaceship/models/mission.py
+++ spaceship/models/mission.py
@@ -32,7 +32,6 @@ class Mission(BaseModel):
     if start:
       clone.started_at = pendulum.now('UTC')
     clone.save()
-    # import here instead of top-level to avoid a cycle
     from .mission_goal import MissionGoal
     for slot in self.goal_slots():
       clone_slot = MissionGoal(mission=clone, goal=slot.goal, week=slot.week)
@@ -40,7 +39,6 @@ class Mission(BaseModel):
     return clone
 
   def goal_slots(self, week=None):
-    # import here instead of top-level to avoid a cycle
     from .mission_goal import MissionGoal
     if week is None:
       return (MissionGoal
@@ -50,3 +48,20 @@ class Mission(BaseModel):
             .select()
             .where((MissionGoal.mission_id == self.id) &
                    (MissionGoal.week == week)))
+
+  def serialize_for_js(self):
+    schedule = []
+    for week in range(1 + self.duration_in_weeks):
+      schedule.append([{'goal_id': g.goal.id} for g in self.goal_slots(week=week)])
+    return {'schedule': schedule}
+
+  def update_from_js(self, state):
+    from .mission_goal import MissionGoal
+    schedule = state['schedule']
+    self.duration_in_weeks = max(0, len(schedule) - 1)
+    MissionGoal.delete().where(MissionGoal.mission_id == self.id).execute()
+    for week_number, week in enumerate(schedule):
+      for slot in week:
+        MissionGoal.insert(mission_id=self.id,
+                           goal_id=slot['goal_id'],
+                           week=week_number).execute()
