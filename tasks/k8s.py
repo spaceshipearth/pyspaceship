523340b9518ba5117f736ef8c5ef1bdd42786224
Igor Serebryany <igor47@moomers.org>
2019-05-16 00:00:31 -0700

    wrap kubectl interactions in a namespace object
    makes sure we always set a namespace correctly when interacting with
    kubernetes

--- tasks/k8s.py
+++ tasks/k8s.py
@@ -4,15 +4,34 @@ from invoke import task, run
 import random
 import string
 
-from . import utils
+from .utils import load_manifest, K8SNamespace, TEST_NAMESPACE
 
-@task
-def pods(ctx):
+@task(
+  help={
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def pods(ctx, namespace=TEST_NAMESPACE):
   """Display existing pods for the service"""
   print('Phase\tImage\tName')
   print('------------------------------')
-  for pod in utils.get_pods():
-    print(f'{pod["phase"]}\t{pod["image"]}\t{pod["name"]}')
+  with K8SNamespace(namespace) as ns:
+    for pod in ns.get_pods():
+      print(f'{pod["phase"]}\t{pod["image"]}\t{pod["name"]}')
+
+@task
+def ingress(ctx):
+  """configure the ingress resource for all namespaces"""
+  raise NotImplementedError('this does not work right now')
+  ingress = load_manifest('ingress')
+  ns.apply(ingress)
+
+  info = json.loads(run('kubectl get ingress pyspaceship-ingress -o=json', hide=True).stdout)
+  ingress = info['status']['loadBalancer']['ingress']
+  print("Load balancer IPs:")
+  for i in ingress:
+    ip = i['ip']
+    print(f'- {ip}')
 
 @task(
   help={
@@ -21,11 +40,19 @@ def pods(ctx):
     'port': 'MySQL port (default: 3306)',
     'username': "MySQL username (default: 'spaceship-app')",
     'db': "Name of the mysql database (default: 'spaceship')",
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
   }
 )
-def mysql_secret(ctx, host, password, port=3306, username = 'spaceship-app', db = 'spaceship'):
+def mysql_secret(
+    ctx,
+    host,
+    password,
+    port=3306,
+    username='spaceship-app',
+    db='spaceship',
+    namespace=TEST_NAMESPACE):
   """Create a secret containing mysql credentials"""
-  secret = utils.load_manifest('mysql_secret', {
+  secret = load_manifest('mysql_secret', {
     'host': host,
     'port': str(port),
     'username': username,
@@ -33,63 +60,97 @@ def mysql_secret(ctx, host, password, port=3306, username = 'spaceship-app', db 
     'db': db,
   })
 
-  utils.k8s_apply(secret, dry_run = False)
+  with K8SNamespace(namespace) as ns:
+    ns.apply(secret)
 
-@task()
-def session_secret(ctx, secret_key = None):
+@task(
+  help={
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def session_secret(ctx, secret_key = None, namespace = TEST_NAMESPACE):
+  """Creates a new session secret; this will invalidate existing sessions"""
   def random_string(length):
     return "".join( random.SystemRandom().choices(string.ascii_letters + string.digits, k=length))
 
   if not secret_key:
     secret_key = random_string(24)
 
-  secret = utils.load_manifest('session_secret', {
+  secret = load_manifest('session_secret', {
     'secret_key': secret_key,
   })
 
-  utils.k8s_apply(secret, dry_run = False)
+  with K8SNamespace(namespace) as ns:
+    ns.apply(secret)
 
-@task()
-def sendgrid_secret(ctx, secret_key = None):
-  secret = utils.load_manifest('sendgrid_secret', {
+@task(
+  help={
+    'secret-key': 'The token from sendgrid',
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def sendgrid_secret(ctx, secret_key, namespace = TEST_NAMESPACE):
+  """The secret to sending email"""
+  secret = load_manifest('sendgrid_secret', {
     'secret_key': secret_key,
   })
 
-  utils.k8s_apply(secret, dry_run = False)
+  with K8SNamespace(namespace) as ns:
+    ns.apply(secret)
 
-@task()
-def google_app_secret(ctx, keyfile):
+@task(
+  help={
+    'keyfile': 'path to the json keyfile',
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def google_app_secret(ctx, keyfile, namespace = TEST_NAMESPACE):
   """upload a keyfile as a google secret
 
   created by:
     gcloud iam service-accounts keys create /tmp/key.json --iam-account=pyspaceship@spaceshipearthprod.iam.gserviceaccount.com
   """
-  secret = utils.load_manifest('google-app-creds', {
+  secret = load_manifest('google-app-creds', {
     'keyfile': open(keyfile).read().strip(),
   })
 
-  utils.k8s_apply(secret, dry_run = False)
+  with K8SNamespace(namespace) as ns:
+    ns.apply(secret)
 
-
-@task()
-def run_migrations(ctx):
+@task(
+  help={
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def run_migrations(ctx, namespace = TEST_NAMESPACE):
   """Migrate the DB that the cluster is connected to"""
-  random_pod_name = utils.random_pod_name()
+  with K8SNamespace(namespace) as ns:
+    random_pod_name = ns.random_pod_name()
 
-  print(f"Using pod {random_pod_name} to run migrations...")
-  run(f'kubectl exec {random_pod_name} inv run.upgrade')
+    print(f"Using pod {random_pod_name} to run migrations...")
+    run(f'{ns.kubecmd} exec {random_pod_name} inv run.upgrade')
 
-@task
-def bounce(ctx):
+@task(
+  help={
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def bounce(ctx, namespace = TEST_NAMESPACE):
   """Bounce the deployment by deleting all pods and allowing them to be recreated"""
-  pod_names = [p['name'] for p in utils.get_pods() if p['phase'] == 'Running']
-  for pod_name in pod_names:
-    run(f'kubectl delete pod {pod_name}')
+  with K8SNamespace(namespace) as ns:
+    pod_names = [p['name'] for p in ns.get_pods() if p['phase'] == 'Running']
+    for pod_name in pod_names:
+      run(f'{ns.kubecmd} delete pod {pod_name}')
 
-@task()
-def shell(ctx):
+@task(
+  help={
+    'namespace': f"Version of the site (default: {TEST_NAMESPACE})",
+  }
+)
+def shell(ctx, namespace = TEST_NAMESPACE):
   """Get a shell on a random pod"""
-  random_pod_name = utils.random_pod_name()
+  with K8SNamespace(namespace) as ns:
+    random_pod_name = ns.random_pod_name()
 
-  print(f"Picked pod {random_pod_name} for shell access...")
-  run(f'kubectl exec -i -t {random_pod_name} -- /bin/bash', pty=True)
+    print(f"Picked pod {random_pod_name} for shell access...")
+    run(f'{ns.kubecmd} exec -i -t {random_pod_name} -- /bin/bash', pty=True)
--- tasks/utils.py
+++ tasks/utils.py
@@ -10,6 +10,68 @@ import yaml
 
 ROOT_REPO_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+DEFAULT_NAMESPACE = 'default'
+TEST_NAMESPACE = 'test'
+PROD_NAMESPACE = 'prod'
+NAMESPACES = [
+  DEFAULT_NAMESPACE,
+  TEST_NAMESPACE,
+  PROD_NAMESPACE,
+]
+
+class K8SNamespace:
+  def __init__(self, namespace, dry_run = False):
+    self.namespace = namespace
+    self.dry_run = dry_run
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.namespace = None
+
+  def get_pods(self):
+    """List running pods"""
+    items = json.loads(
+      run(f'{self.kubecmd} get pods -l app=pyspaceship -o=json', hide=True).stdout
+    )['items']
+
+    pods = []
+    for item in items:
+      pods.append({
+        'name': item['metadata']['name'],
+        'image': item['status']['containerStatuses'][0]['image'],
+        'phase': item['status']['phase'],
+      })
+
+    return pods
+
+  def random_pod_name(self):
+    return [p['name'] for p in self.get_pods() if p['phase'] == 'Running'].pop()
+
+  @property
+  def kubecmd(self):
+    if self.namespace not in NAMESPACES:
+      raise ValueError(f"invalid namespace '{self.namespace}'")
+
+    return f"kubectl --namespace={self.namespace}"
+
+  def apply(self, manifest):
+    """run `kubectl apply` on the specified manifest"""
+    # write out the modified config
+    with tempfile.NamedTemporaryFile(mode='w') as tf:
+      yaml.dump(manifest, tf)
+      tf.flush()
+
+      if self.dry_run:
+        print("The config we generated:")
+        print(open(tf.name).read())
+        run(f'{self.kubecmd} apply --dry-run -f {tf.name}')
+
+      else:
+        run(f'{self.kubecmd} apply -f {tf.name}')
+
+
 @contextlib.contextmanager
 def in_repo_root():
   cwd = os.getcwd()
@@ -28,38 +90,3 @@ def load_manifest(mtype, context = {}):
   with open(manifest_path) as mt:
     manifest_template = yaml.safe_load(mt.read())
     return jsone.render(manifest_template, context)
-
-def k8s_apply(manifest, dry_run = True):
-  """run `kubectl apply` on the specified manifest"""
-  # write out the modified config
-  with tempfile.NamedTemporaryFile(mode='w') as tf:
-    yaml.dump(manifest, tf)
-    tf.flush()
-
-    if dry_run:
-      print("The config we generated:")
-      print(open(tf.name).read())
-      run('kubectl apply --dry-run -f %s' % tf.name)
-
-    else:
-      run('kubectl apply -f %s' % tf.name)
-
-def get_pods():
-  """List running pods"""
-  items = json.loads(
-    run('kubectl get pods -l app=pyspaceship -o=json', hide=True).stdout
-  )['items']
-
-  pods = []
-  for item in items:
-    pods.append({
-      'name': item['metadata']['name'],
-      'image': item['status']['containerStatuses'][0]['image'],
-      'phase': item['status']['phase'],
-    })
-
-  return pods
-
-def random_pod_name():
-  return [p['name'] for p in get_pods() if p['phase'] == 'Running'].pop()
-
