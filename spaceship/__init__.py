8c87b8808b8b36a5a6d6efcfb2598fdf39db4e3a
Christopher Lin <powertothepenguins@gmail.com>
2019-05-24 13:49:26 -0700

    Configure assets to include version hash in filename
    Previously, if you edited an asset file, the server wouldn't deliver the
    updated version, so you'd have to hard-refresh. This configures the
    application to append a version hash to the asset filename so that
    asset updates are requested and delivered as one would expect.

--- spaceship/__init__.py
+++ spaceship/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
+
 app = Flask(__name__)
+assets = Environment(app)
+
+js = Bundle('edit.js',
+            'prevent-invalid-form-submit.js',
+            'wip-overlay.js',
+            output='gen/all.%(version)s.js')
+assets.register('js_all', js)
 
 from .config import Config
 app.config.from_object(Config)
