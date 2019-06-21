0bccb38a1e0dabde7f1e82512c9125ff047a5f09
Igor Serebryany <igor47@moomers.org>
2019-06-20 17:39:00 -0700

    reorganize main app __init__ imports
    the order of things matters

--- spaceship/__init__.py
+++ spaceship/__init__.py
@@ -1,20 +1,29 @@
-from flask import Flask
 
+# create the flask app
+from flask import Flask
 app = Flask(__name__)
 
+# configure the app
 from spaceship.config import Config
 app.config.from_object(Config)
 
+# configure logging
 from spaceship import logs
 
+# CSRF protection
 from flask_wtf.csrf import CSRFProtect
 csrf = CSRFProtect(app)
 
+# handling of redirects and static files
 from spaceship import assets
 from spaceship import enforce_ssl
 from spaceship import enforce_www
-from spaceship import google_auth
+
+# load the db
 from spaceship import db
+
+# these things depend on the db and so go after it
 from spaceship import login
+from spaceship import google_auth
 from spaceship import views
 
