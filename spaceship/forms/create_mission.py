e8a813ca2811e469fb9c4970cc87059be3850a09
Igor Kofman <kofman@gmail.com>
2019-06-24 17:56:01 -0700

    Improve crew page: display goals, refine UI

--- spaceship/forms/create_mission.py
+++ spaceship/forms/create_mission.py
@@ -2,8 +2,8 @@
 from flask_wtf import FlaskForm
 import pendulum
 import wtforms
-
 from spaceship.forms.pendulum_fields import DateField
+from spaceship.models.goal import GOALS
 
 def next_monday():
   now = pendulum.now('UTC')
@@ -19,11 +19,9 @@ class CreateMissionForm(FlaskForm):
   def __init__(self, team_id):
     super(CreateMissionForm, self).__init__()
     self.team_id.data = team_id
-
-  goal = wtforms.SelectField(label='Mission goal',
-                          choices=[('beefless', 'Eat no beef'),
-                                  ('vegeterian', 'Eat a vegeterian diet'),
-                                  ('vegan', 'Eat a vegan diet')])
+  
+  goal = wtforms.SelectField(label='Mission goal', 
+                           choices=[(goal.short_description, goal.short_description) for goal in GOALS['diet']])
   start = DateField(label='Mission start date', default=next_monday)
   duration = wtforms.SelectField(label='Mission duration', choices=[('1', 'One week')])
   create = wtforms.SubmitField('Create Mission')
