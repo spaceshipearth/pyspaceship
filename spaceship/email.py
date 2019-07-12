6cbb1d52ac828b467ca7d1bfbdc9d45ee4f6a42d
Igor Serebryany <igor47@moomers.org>
2019-07-11 20:33:20 -0700

    email: properly parse mission start/end times
    we were passing strings, not pendulum instances, through celery

--- spaceship/email.py
+++ spaceship/email.py
@@ -1,14 +1,14 @@
 import logging
 from flask import render_template
+import pendulum
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 from spaceship import app
+from spaceship.celery import celery
 from spaceship.models import Mission
 
 log = logging.getLogger('spaceship.email')
-
-from .celery import celery
 
 @celery.task
 def send(to_emails, subject, html_content, from_email='gaia@spaceshipearth.org'):
@@ -30,28 +30,33 @@ def send(to_emails, subject, html_content, from_email='gaia@spaceshipearth.org')
 def schedule_mission_emails(mission):
   """sets mission start/end emails to be sent at the correct time"""
   send_mission_start.apply_async(
-    args=(mission.id, mission.started_at),
+    args=(mission.id, mission.started_at.isoformat()),
     eta=mission.started_at,
     expires=mission.started_at.add(days=1),  # don't bother sending past a day late
   )
 
   send_mission_end.apply_async(
-    args=(mission.id, mission.end_time),
+    args=(mission.id, mission.end_time.isoformat()),
     eta=mission.end_time,
     expires=mission.end_time.add(days=1),    # don't bother sending past a day late
   )
 
 @celery.task
-def send_mission_start(mission_id, started_at):
+def send_mission_start(mission_id, planned_start_str):
   mission = Mission.query.get(mission_id)
-  if not (mission and mission.is_active):
+  planned_start = pendulum.parse(planned_start_str)
+
+  # mission should be active
+  if (not mission) or mission.is_deleted:
     log.warning(f'not sending mission start email for inactive mission {mission_id}')
     return
 
   # the start time has changed, so a different thing will actually
   # send the email
-  if mission.started_at != started_at:
-    log.warning(f'not sending mission start email for {mission} since start time has changed')
+  if mission.started_at != planned_start:
+    log.warning(
+      f"not sending mission start email for {mission} since start time has changed "\
+      f"({mission.started_at} from {planned_start})")
     return
 
   # if we got here, we should actually send an email
@@ -66,16 +71,20 @@ def send_mission_start(mission_id, started_at):
   )
 
 @celery.task
-def send_mission_end(mission_id, end_time):
+def send_mission_end(mission_id, planned_end_str):
   mission = Mission.query.get(mission_id)
+  planned_end = pendulum.parse(planned_end_str)
+
   if not (mission and mission.is_active):
     log.warning(f'not sending mission end email for inactive mission {mission_id}')
     return
 
   # the end time has changed, so a different thing will actually
   # send the email
-  if mission.end_time != end_time:
-    log.warning(f'not sending mission end email for {mission} since end time has changed')
+  if mission.end_time != planned_end:
+    log.warning(
+      f"not sending mission end email for {mission} since end time has changed "\
+      f"({mission.end_time} from {planned_end})")
     return
 
   # if we got here, we should actually send an email
