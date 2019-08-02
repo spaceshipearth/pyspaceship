1cc2a0b786995f183aa844755cd24b45c389821a
Igor Serebryany <igor@aclima.io>
2019-08-01 21:35:03 -0700

    avoid celery getting into a weird db state
    been seeing errors like this:
    ```
    _revalidate_connection "Can't reconnect until invalid " sqlalchemy.exc.StatementError: (sqlalchemy.exc.InvalidRequestError) Can't reconnect until invalid transaction is rolled back
    ```
    
    and like this:
    ```
    sqlalchemy.exc.InvalidRequestError: Can't reconnect until invalid transaction is rolled back
    ```
    
    this happens if there's some error in a celery task. this breaks mysql
    in that worker forever (because workers only initialize the DB once, at
    startup).
    
    we add a hook to auto-roll back the session if there's an error in the
    task, or auto-commit it if there's no error, and then remove the session
    no matter what at the end of the task. this allows our workers to
    continue processing tasks even if something goes wrong with one of them.

--- spaceship/celery.py
+++ spaceship/celery.py
@@ -1,9 +1,24 @@
 
 from celery import Celery
 
-from . import app
+from spaceship import app
+from spaceship.db import db as sqlalchemy_db
+
 host, port, db = app.config['REDIS_HOST'], app.config['REDIS_PORT'], app.config['REDIS_DB']
 
 celery = Celery(app.name, broker=f"redis://{host}:{port}/{db}")
 celery.conf.task_serializer = 'json'
 celery.conf.always_eager = not app.config['USE_CELERY_WORKERS']
+
+class SqlAlchemyTask(celery.Task):
+    """An abstract Celery Task that ensures that the connection the the
+    database is closed on task completion"""
+    abstract = True
+
+    def after_return(self, status, retval, task_id, args, kwargs, einfo):
+        if einfo:
+            sqlalchemy_db.session.rollback()
+        else:
+            sqlalchemy_db.session.commit()
+
+        sqlalchemy_db.session.remove()
--- spaceship/email.py
+++ spaceship/email.py
@@ -6,7 +6,7 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 from spaceship import app
-from spaceship.celery import celery
+from spaceship.celery import celery, SqlAlchemyTask
 from spaceship.models import Mission
 
 log = logging.getLogger('spaceship.email')
@@ -48,7 +48,7 @@ def schedule_mission_emails(mission):
     expires=mission.end_time.add(days=1),    # don't bother sending past a day late
   )
 
-@celery.task
+@celery.task(base=SqlAlchemyTask)
 def send_mission_start(mission_id, planned_start_str):
   mission = Mission.query.get(mission_id)
   planned_start = pendulum.parse(planned_start_str)
@@ -79,7 +79,7 @@ def send_mission_start(mission_id, planned_start_str):
     html_content=content,
   )
 
-@celery.task
+@celery.task(base=SqlAlchemyTask)
 def send_mission_end(mission_id, planned_end_str):
   mission = Mission.query.get(mission_id)
   planned_end = pendulum.parse(planned_end_str)
