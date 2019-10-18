974b15e03ea76af19e12730e2be85f8e93417a59
Igor Serebryany <igor47@moomers.org>
2019-10-17 20:36:56 -0700

    placeholder for a bunch of goals

---
+++ spaceship/goals.py
@@ -0,0 +1,46 @@
+
+GOALS_BY_CATEGORY = {
+  "diet": {
+    "icon": None,
+    "description": None,
+    "goals": [
+      {
+        "name": "Go Vegan",
+        "short_description": "Avoid all animal products",
+        "description": "Animal products are bad. Don't eat them.",
+      },
+      {
+        "name": "Go Vegetarian",
+        "short_description": "Avoid all animal products",
+        "description": "Animal products are bad. Don't eat them.",
+      },
+      {
+        "name": "Avoid Beef",
+        "short_description": "Avoid all animal products",
+        "description": "Animal products are bad. Don't eat them.",
+      },
+    ],
+  },
+  "transportation": {
+    "icon": None,
+    "description": None,
+    "goals": [
+      {
+        "name": "Bike to work",
+        "short_description": "Ride your bike to work or school",
+        "description": "You should be biking more. Good for you and good for the planet.",
+      },
+    ],
+  },
+  "education": {
+    "icon": None,
+    "description": None,
+    "goals": [
+      {
+        "name": "Read a book",
+        "short_description": "Become educated about climate change",
+        "description": "This is your chance. You've always wanted to read.",
+      },
+    ],
+  },
+}
