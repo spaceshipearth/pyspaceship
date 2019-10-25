2e477fc5fb94df04d87dca4d7525bb6f7215aefe
Igor Serebryany <igor47@moomers.org>
2019-10-24 21:48:16 -0700

    fixup! delete CreateCrew form/view

--- spaceship/forms/__init__.py
+++ spaceship/forms/__init__.py
@@ -1,5 +1,4 @@
 
-from spaceship.forms.create_crew import CreateCrew
 from spaceship.forms.enlist import AcceptInvitation, DeclineInvitation
 from spaceship.forms.login import Login
 from spaceship.forms.register import Register
